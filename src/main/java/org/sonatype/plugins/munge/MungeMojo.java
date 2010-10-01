0dadb1b2e47488977a48b785ef966f45c7bd8cc1
Stuart McCulloch <mcculls@gmail.com>
2010-10-01 12:41:13 +0100

    Skeleton mojo project

---
+++ src/main/java/org/sonatype/plugins/munge/MungeMojo.java
@@ -0,0 +1,20 @@
+package org.sonatype.plugins.munge;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Munge.
+ *
+ * @goal munge
+ *
+ * @phase process-sources
+ */
+public class MungeMojo
+    extends AbstractMojo
+{
+    public void execute()
+        throws MojoExecutionException
+    {
+    }
+}
